58b9df22a827673466079124be31960a28885e31
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-12-09 13:49:15 +0000

    chores: Working on components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     content: ["node_modules/preline/dist/*.js"],
     plugins: [require("preline/plugin")],
+    theme: {
+        colors: {
+            primary: "#FB9345",
+            secondary: "#FFB534",
+            success: "#67BA79",
+            warning: "#F8CC5C",
+            danger: "#F26B4E",
+            info: "#DDDDDD",
+        },
+    },
 };
