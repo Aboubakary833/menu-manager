790ec8cca2cb89026009600dae334d15c3be78cc
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-12-16 16:24:02 +0000

    feat: Added spatie/laravel-permission

---
+++ app/Models/Permission.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Permission as SpatiePermissionModel;
+
+class Permission extends SpatiePermissionModel
+{
+    use HasFactory, HasUlids;
+}
