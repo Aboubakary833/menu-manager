07654fa3932b0d5811e5f7c7bdec5d1bb9e01d69
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-01-08 19:38:20 +0000

    chores: Working JSX Email building artisan command

--- app/Console/Commands/BuildJSXEmail.php
+++ app/Console/Commands/BuildJSXEmail.php
@@ -3,6 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use SplFileInfo;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class BuildJSXEmail extends Command
 {
@@ -11,20 +15,75 @@ class BuildJSXEmail extends Command
      *
      * @var string
      */
-    protected $signature = 'app:build-j-s-x-email';
+    protected $signature = 'jsx_email:build {name}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Build a JSX Email template';
+
+    protected string $componentsDir = "resources/ts/emails/templates";
+
+    protected string $outDir = "resources/views/mails";
+
+    protected array $componentsRegistry;
+
+    public function __construct(
+        protected Filesystem $filesystem,
+    ) {
+        parent::__construct();
+        $files = $this->filesystem->files(
+        base_path($this->componentsDir)
+      );
+        $this->componentsRegistry = $this->getComponentsNames($files);
+    }
 
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle() : void
     {
-        //
+        if (!$this->componentExist())
+            return;
+        $filename = $this->argument("name") . ".tsx";
+        $this->buildTemplate($filename);
+    }
+
+    /**
+     * @param SplFileInfo[] $files
+     * @return array<string>
+     */
+    public function getComponentsNames(array $files) : array
+    {
+        return array_map(
+            fn(SplFileInfo $file) => str_replace(".tsx", '', $file->getFilename()),
+            $files
+        );
+    }
+
+    public function componentExist() : bool
+    {
+        $argument = $this->argument("name");
+        if (!in_array($argument, $this->componentsRegistry))
+        {
+            $this->error(
+                sprintf("Component \"%s\" not found in %s.", $argument, $this->componentsDir)
+            );
+            return false;
+        }
+        return true;
+    }
+
+    public function buildTemplate(string $filename) : void
+    {
+        $fullPath = base_path($this->componentsDir . "/" . $filename);
+        $output = new BufferedOutput();
+        $input = new StringInput(
+            sprintf("email build %s", $fullPath)
+        );
+        dd($this->execute($input, $output));
+        dd($output);
     }
 }
