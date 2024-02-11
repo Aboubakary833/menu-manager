b9b94aa4be1d695c2a9aad749bd0828f69eb1475
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-02-11 15:31:04 +0000

    done: Passwordless login feature

--- app/Http/Controllers/Auth/Login/ConfirmIdentityController.php
+++ app/Http/Controllers/Auth/Login/ConfirmIdentityController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers\Auth\Login;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ConfirmIdentityRequest;
+use App\Services\Auth\LoginService;
 
 class ConfirmIdentityController extends Controller
 {
+
+    public function __construct(public LoginService $service) {}
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(ConfirmIdentityRequest $request)
     {
-        dd($request->getCode());
+        $this->service->authUserWithCode($request);
+        return to_route("dashboard");
     }
 }
